8dea3956b4ea5a97af22d62cba4c9e0b0f9b4427
David M. Hollenstein <hollenstein.david@gmail.com>
2020-06-15 11:34:44 +0200

    Added scripts to process the MaxQuant evidence files.

---
+++ scripts/process_evidence_HTB-Gis1-Rph1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+import max_quant as MQ
+
+
+# Process the MaxQuant results of the Gis1 and Rph1 HTB purification experiments
+if __name__ == '__main__':
+    root = '../'
+    table_dir = os.path.join(root, 'tables')
+    fasta_path = os.path.join(root, 'yeast_cont_20140324.fasta')
+    mq_txt_folder = 'MQ_txt_HTB-Gis1-Rph1'
+    evidence_path = os.path.join(
+        root, 'MaxQuant', mq_txt_folder, 'evidence.txt'
+    )
+    setup = 'htb-gis1-rph1'
+
+    # Import Evidence
+    evidence, normalization_data = MQ.process_evidence(evidence_path, fasta_path)
+    evidence['Setup'] = evidence['Experiment'].apply(lambda e: e.split('_')[0])
+    experiments_toreverse = np.unique(evidence['Experiment'])
+    MQ.reverse_ratios(evidence, experiments_toreverse)
+    MQ.normalize_protein_abundance(evidence, 'GIS1')
+    MQ.normalize_protein_abundance(evidence, 'RPH1')
+
+    # Generate output files
+    evidence_outpath = os.path.join(table_dir, 'evidence_' + setup + '.tsv')
+    phosphosite_outpath = os.path.join(table_dir, 'phospho_' + setup + '.tsv')
+    protein_outpath = os.path.join(table_dir, 'protein_' + setup + '.tsv')
+
+    MQ.write_evidence_table(evidence, evidence_outpath)
+    MQ.write_phosphosite_table(evidence, phosphosite_outpath, prob_cutoff=0.7)
+    MQ.write_protein_table(evidence, protein_outpath)
